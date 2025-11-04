df3ae3c1088f919cfd38677cc5b4bd99d384e68f
yusuke-taniguchi0510 <191002487+yusuke-taniguchi0510@users.noreply.github.com>
2025-11-04 22:51:50 +0900

    Mod: 最小構成でリファクタリング

--- src/section8/no2/sample1/Item.java
+++ src/section8/no2/sample1/Item.java
@@ -1,20 +1,4 @@
 package section8.no2.sample1;
 
 @Test(name = "test", price = 100)
-public class Item {
-    private final String name;
-    private final int price;
-
-    public Item(String name, int price) {
-        this.name = name;
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-}
+public class Item {}
