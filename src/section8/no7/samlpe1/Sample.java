08185f57022a4c0c04ad809cfc1f93839bc79f5d
yusuke-taniguchi0510 <191002487+yusuke-taniguchi0510@users.noreply.github.com>
2025-11-06 01:26:59 +0900

    Update: javac Sample.javaで警告OFF

---
+++ src/section8/no7/samlpe1/Sample.java
@@ -0,0 +1,12 @@
+package section8.no7.samlpe1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sample {
+    @SuppressWarnings("unchecked")
+    public void sample() {
+        List list = new ArrayList();
+        list.add("hello");
+    }
+}
