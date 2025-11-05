e868268cb96ebaf24d6da9fe8040080e93a58040
yusuke-taniguchi0510 <191002487+yusuke-taniguchi0510@users.noreply.github.com>
2025-11-06 01:19:27 +0900

    Update: Overrideアノテーション

---
+++ src/section8/no5/A.java
@@ -0,0 +1,7 @@
+package section8.no5;
+
+public class A {
+    public void hello(String name) {
+        System.out.println("hello" + name);
+    }
+}
---
+++ src/section8/no5/B.java
@@ -0,0 +1,8 @@
+package section8.no5;
+
+public class B extends A {
+    @Override
+    public void hello(String name) {
+        System.out.println("hi. " + name);
+    }
+}
