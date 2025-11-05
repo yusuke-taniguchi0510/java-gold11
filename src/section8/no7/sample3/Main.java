8c736af9556e194db9cadce1cf4aa261c6bc8566
yusuke-taniguchi0510 <191002487+yusuke-taniguchi0510@users.noreply.github.com>
2025-11-06 01:31:26 +0900

    Update: @SuppressWarnings("deprecation")で非推奨の警告を抑制

---
+++ src/section8/no7/sample3/Main.java
@@ -0,0 +1,8 @@
+package section8.no7.sample3;
+
+public class Main {
+    @SuppressWarnings("deprecation") // 非推奨の警告を抑制
+    public static void main(String[] args) {
+        new Test().test();
+    }
+}
---
+++ src/section8/no7/sample3/Test.java
@@ -0,0 +1,8 @@
+package section8.no7.sample3;
+
+public class Test {
+
+    @Deprecated // testメソッドが非推奨であることを明示
+    public void test() {
+    }
+}
