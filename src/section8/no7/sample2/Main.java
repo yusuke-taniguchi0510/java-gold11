57546e878f4dda8e32ccf16f813f071e6b83e2b1
yusuke-taniguchi0510 <191002487+yusuke-taniguchi0510@users.noreply.github.com>
2025-11-06 01:29:29 +0900

    Update: @Deprecatedで非推奨であることを明示

---
+++ src/section8/no7/sample2/Main.java
@@ -0,0 +1,7 @@
+package section8.no7.sample2;
+
+public class Main {
+    public static void main(String[] args) {
+        new Test().test();
+    }
+}
---
+++ src/section8/no7/sample2/Test.java
@@ -0,0 +1,8 @@
+package section8.no7.sample2;
+
+public class Test {
+
+    @Deprecated // testメソッドが非推奨であることを明示
+    public void test() {
+    }
+}
